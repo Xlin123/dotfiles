ceea1c7a703dbed1b908d1543416ca2cd10ccc2f
Xlin123 <xavier.li.1332@gmail.com>
2026-01-14 18:15:41 -0500

    chore: remove slop

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,33 +1,3 @@
--- Enhanced Native Neovim Configuration
--- 
--- Features:
---   • Mason for LSP server management
---   • Blink.cmp for fast completion
---   • Telescope for fuzzy finding
---   • Fugitive + Gitsigns for Git integration
---   • Oil.nvim for file exploration
---   • Quality of life plugins (Comment, Autopairs, Surround, etc.)
---
--- First run:
---   1. Open Neovim - plugins will auto-install
---   2. Run :Mason to install LSP servers
---   3. Restart Neovim
---
--- Key bindings:
---   <Space> = Leader key
---   <Space>pf = Find files
---   <Ctrl-p> = Git files
---   <Space>ps = Live grep
---   - = Open Oil file explorer
---   <Space>gs = Git status (Fugitive)
---   gcc = Comment line
---   ys<motion><char> = Surround
---   <Space>m = Open Mason
---
--- See individual config files in lua/board/ for more details
-
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
 vim.opt.winblend = 10
 vim.opt.termguicolors = true
 
